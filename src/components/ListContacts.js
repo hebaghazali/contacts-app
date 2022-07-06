c4e9868a2648c604717b55f95df84a4ae203b3b1
Heba Ghazaly <hebbaghazaly@gmail.com>
2022-07-06 10:28:07 +0200

    Passing data with props

---
+++ src/components/ListContacts.js
@@ -0,0 +1,25 @@
+const ListContacts = ({ contacts }) => {
+  return (
+    <ol className='contact-list'>
+      {contacts.map(contact => {
+        const { id, name, handle, avatarURL } = contact;
+
+        return (
+          <li key={id} className='contact-list-item'>
+            <div
+              className='contact-avatar'
+              style={{ backgroundImage: `url(${avatarURL})` }}
+            ></div>
+            <div className='contact-details'>
+              <p>{name}</p>
+              <p>{handle}</p>
+            </div>
+            <button className='contact-remove'>Remove</button>
+          </li>
+        );
+      })}
+    </ol>
+  );
+};
+
+export default ListContacts;
