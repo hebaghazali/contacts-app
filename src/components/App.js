cc1b60b47d0c19e9219d59c5c1f4e007d48f69e8
Heba Ghazaly <hebbaghazaly@gmail.com>
2022-07-16 23:17:57 +0200

    Finishing the Contact Form

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,28 @@
 import { useState, useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import '../css/App.css';
 import ListContacts from './ListContacts';
 import * as ContactsAPI from '../utils/ContactsAPI';
 import CreateContact from './CreateContact';
 
 const App = () => {
+  let navigate = useNavigate();
+
   const removeContact = contact => {
     ContactsAPI.remove(contact);
 
     setContacts(contacts.filter(c => c.id !== contact.id));
+  };
+
+  const createContact = contact => {
+    const create = async () => {
+      const res = await ContactsAPI.create(contact);
+
+      setContacts(contacts.concat(res));
+    };
+
+    create();
+    navigate('/');
   };
 
   const [contacts, setContacts] = useState([]);
@@ -32,7 +45,16 @@ const App = () => {
           <ListContacts contacts={contacts} onDeleteContact={removeContact} />
         }
       />
-      <Route path='/create' element={<CreateContact />} />
+      <Route
+        path='/create'
+        element={
+          <CreateContact
+            onCreateContact={contact => {
+              createContact(contact);
+            }}
+          />
+        }
+      />
     </Routes>
   );
 };
--- src/components/CreateContact.js
+++ src/components/CreateContact.js
@@ -1,5 +1,39 @@
-const CreateContact = () => {
-  return <div>Create Contact</div>;
+import { Link } from 'react-router-dom';
+import ImageInput from './ImageInput';
+import serializeForm from 'form-serialize';
+
+const CreateContact = ({ onCreateContact }) => {
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const values = serializeForm(e.target, { hash: true });
+
+    if (onCreateContact) {
+      onCreateContact(values);
+    }
+  };
+
+  return (
+    <div>
+      <Link className='close-create-contact' to='/'>
+        Close
+      </Link>
+
+      <form onSubmit={handleSubmit} className='create-contact-form'>
+        <ImageInput
+          className='create-contact-avatar-input'
+          name='avatarURL'
+          maxHeight={64}
+        />
+
+        <div className='create-contact-details'>
+          <input type='text' name='name' placeholder='name' />
+          <input type='text' name='handle' placeholder='handle' />
+          <button>Add Contact</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default CreateContact;
